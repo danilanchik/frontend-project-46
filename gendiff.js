2c9475347ca172a46e6c8dfe0fe1e4f6999624f0
danilanchik <danilanchik@gmail.com>
2023-07-06 23:14:44 +0300

    step 2

--- gendiff.js
+++ gendiff.js
@@ -4,7 +4,13 @@ const program = require('commander');
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0') // Укажите актуальную версию вашего приложения
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format')
   .helpOption('-h, --help', 'output usage information')
   .parse(process.argv);
 
+const options = program.opts();
+const filePaths = program.args;
+
+console.log(options);
+console.log(filePaths);
