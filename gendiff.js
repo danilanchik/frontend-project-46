e7b571078a976f6100f569b1d9492900ade701bb
danilanchik <danilanchik@gmail.com>
2023-07-06 23:00:44 +0300

    step 1

--- gendiff.js
+++ gendiff.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-const { program } = require('commander');
+const program = require('commander');
 
 program
   .description('Compares two configuration files and shows a difference.')
