e68bcd5cb012d492703fe746cc75e3667ffb37cd
danilanchik <danilanchik@gmail.com>
2023-07-18 20:36:44 +0300

    step 8 v11

--- src/getDiff.js
+++ src/getDiff.js
@@ -1,7 +1,7 @@
 import _ from 'lodash';
 
 const getDiff = (data1, data2) => {
-  const keys = _.union(Object.keys(data1), Object.keys(data2)).slice().sort();
+  const keys = _.sortBy(_.union(Object.keys(data1), Object.keys(data2)));
   return keys.map((key) => {
     if (!_.has(data1, key)) {
       return { name: key, status: 'added', value: data2[key] };
